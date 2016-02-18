52bf37eb05083ad4ae940f71a8b9b46593c51fab
Joe Barrow <joseph.d.barrow@gmail.com>
2016-02-18 16:16:51 -0500

    Cleaning up more

--- loader.py
+++
@@ -1,4 +0,0 @@
-from pandas import DataFrame
-
-def parse_file(filename):
-    df = DataFrame.from_csv(filename, sep='\t')
--- preprocess.py
+++
@@ -1,6 +0,0 @@
-
-dirs = ['SptkBGAm']
- subs = ['ISOL', 'RAND', 'UCHO', 'MUS']
-
-if __name__ == '__main__':
-    print "Preprocessing..."
