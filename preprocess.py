8174f0dac3a7dafed0ac947433a857c6455ed7d4
Joe Barrow <joseph.d.barrow@gmail.com>
2016-02-22 20:13:25 -0500

    Worked on preprocessing and added training

--- preprocess.py
+++ preprocess.py
@@ -3,10 +3,18 @@ from preprocessing.filterbank import FilterBank
 from preprocessing.band import Band
 
 import numpy as np
+import argparse
 
-print "Constructing filterbank."
-f = FilterBank(44100, 0.05, 5)
-f.construct_bands(440.0, 20, 20)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', dest='recursive_top_dir')
 
-print "Loading and preprocessing training data, MAPS_MUS-alb_se7_AkPnBsdf."
-audio = TrainingData('data/MUS/MAPS_MUS-alb_se7_AkPnBsdf', f)
+    args = parser.parse_args()
+    rdir = args.recursive_top_dir
+
+    print "Constructing filterbank."
+    f = FilterBank(44100, 0.05, 5)
+    f.construct_bands(440.0, 20, 20)
+
+    print "Loading and preprocessing training data, MAPS_MUS-alb_se7_AkPnBsdf."
+    audio = TrainingData('data/MUS/MAPS_MUS-alb_se7_AkPnBsdf', f)
