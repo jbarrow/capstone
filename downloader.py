c2bc3c66abaddef3d468a873bb1a9192d31b5f9c
Joe Barrow <joseph.d.barrow@gmail.com>
2016-02-22 20:53:54 -0500

    Added --new option to downloader

--- downloader.py
+++ downloader.py
@@ -15,7 +15,19 @@ def get_file_list(dir_output):
         l = line.split()
         files.append((l[-1], l[4]))
 
-def download(all_files=False):
+def get_downloaded_list():
+    downloaded = []
+    for f in os.listdir('./downloads/'):
+        if os.path.isfile('./downloads/'+f) and f.endswith('.zip'):
+            downloaded.append((f, 0))
+    return downloaded
+
+def in_downloads(downloaded, filename):
+    for f, s in downloaded:
+        if f == filename: return True
+    return False
+
+def download(downloaded, all_files=False):
     # Connect to the MAPS ftp server over FTPS
     ftps = FTP_TLS('ftps.tsi.telecom-paristech.fr')
     print 'Connected to MAPS FTP over TLS.'
@@ -26,13 +38,21 @@ def download(all_files=False):
         print "Incorrect username/password" ; quit
 
     ftps.retrlines('LIST *.zip', get_file_list)
+
+    if len(files) == len(downloaded):
+        print "All MAPS files downloaded. Continuing."
+        return
     
     if all_files:
         for f, s in files:
-            print "Downloading", f, "of size", s, "bytes"
-            res = ftps.retrbinary('RETR '+f, open('./downloads/'+f, 'wb').write)
+            if not in_downloads(downloaded, f):
+                print "Downloading", f, "of size", s, "bytes"
+                res = ftps.retrbinary('RETR '+f, open('./downloads/'+f, 'wb').write)
     else:
         f, s = random.choice(files)
+        while in_downloads(downloaded, f):
+            f, s = random.choice(files)
+        
         print "Downloading", f, "of size", s, "bytes"
         res = ftps.retrbinary('RETR '+f, open('./downloads/'+f, 'wb').write)
 
@@ -54,18 +74,17 @@ if __name__ == '__main__':
     # Set up our command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--all', dest='download_all', action='store_true')
+    parser.add_argument('--new', dest='download_new', action='store_true')
     
     args = parser.parse_args()
     allf = args.download_all
+    newf = args.download_new
 
     # Check for previous downloads
-    downloaded = []
-    for f in os.listdir('./downloads/'):
-        if os.path.isfile('./downloads/'+f) and f.endswith('.zip'):
-            downloaded.append((f, 0))
-    if len(downloaded) > 0: print 'Detected MAPS files download.'
-    else: downloaded = download(allf)
-
+    downloaded = get_downloaded_list()
+    if len(downloaded) > 0 and not newf: print 'Detected MAPS files download.'
+    else: downloaded = download(downloaded, allf)
+    
     # Unzip all our downloaded files
     for d, _ in downloaded:
         key = d.split('_')[1]
