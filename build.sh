90f51989b3230f809f5e579eff6c2e5083d56a67
Joseph Barrow <joseph.d.barrow@gmail.com>
2016-03-27 22:09:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+# Download ALL of MAPS
 python download.py --all
+
+# Preprocess only the monophonic portion
 python preprocess.py --dir ./data/ISOL
-python train.py
+
+# Ignore if this throws an error. Just being safe.
+cp ~/.theanorc ~/.theanorc-old
+
+# Create the necessary ~/.theanorc file
+echo "[global]
+floatX = float32
+device = gpu
+
+[nvcc]
+fastmath = True" > ~/.theanorc
+
+# We have to use `sudo` so Theano will use the GPU.
+sudo python train.py
