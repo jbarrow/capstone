ea3579b70532838a1cf877139c7fd967d7e0b2c3
Joe Barrow <joseph.d.barrow@gmail.com>
2016-04-14 15:42:40 -0400

    Working on TIMIT model

--- timit_train.py
+++ timit_train.py
@@ -1,5 +1,4 @@
 import numpy as np
-import h5py
 
 from data import DataContainer
 from keras.layers.core import TimeDistributedDense, Dropout, Activation
@@ -13,20 +12,24 @@ batch_size=100
 np.random.seed(1994)
 
 print "Loading data..."
-data = DataContainer('data.h5', in_memory=True)
+data = DataContainer('timit.h5', in_memory=True)
 
 def model_factory():
-    print "Assembling model..."
+    model.add(TimeDistributedDense(input_dim=801, output_dim=256))
+    model.add(Activation('sigmoid'))
+
+    model.add(TimeDistributedDense(input_dim=256, output_dim=256))
+    model.add(Activation('relu'))
+    
     model = Sequential()
     model.add(
         LSTM(
-            input_dim=2206, output_dim=256,
-            return_sequences=True, activation='tanh',
-            dropout_U=0.25, dropout_W=0.25, W_regularizer='l2'
+            input_dim=256, output_dim=256,
+            return_sequences=True
         )
     )
-    
-    model.add(TimeDistributedDense(input_dim=256, output_dim=89))
+
+    model.add(TimeDistributedDense(input_dim=256, output_dim=63))
     model.add(Activation('softmax'))
 
     print "Compiling model..."
@@ -34,20 +37,20 @@ def model_factory():
 
     return model
 
-checkpoint = ModelCheckpoint('models/weights.{epoch:02d}-{val_loss:.2f}.h5')
+checkpoint = ModelCheckpoint('models/timit_weieghts.h5')
+print "Assembling model..."
 model = model_factory()
-print "Fitting model..."
 
 model.fit(
     data.X_train, data.y_train,
     batch_size=batch_size, nb_epoch=nb_epoch,
     show_accuracy=True, verbose=1,
-    validation_split=0.1, callbacks=[checkpoint]
+    validation_split=1, callback=[checkpoint]
 )
 
 print "Saving fitted model..."
 json_string = model.to_json()
-open('models/model_cqt.json', 'w').write(json_string)
-model.save_weights('models/model_cqt.h5', overwrite=True)
+open('models/timit.json', 'w').write(json_string)
+model.save_weights('models/timit.h5', overwrite=True)
 
 data.close()
