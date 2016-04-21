ac92aba259328438064297c13f85e90a5405bf06
Harang Ju <harang.ju@willowtreeapps.com>
2016-04-21 00:40:29 -0400

    refactor

--- construct_song_hmm.py
+++ construct_song_hmm.py
@@ -6,6 +6,8 @@ from stairway import Stairway
 from stairway.steps import stft
 from keras.models import model_from_json
 from pomegranate import *
+from itertools import groupby
+from operator import itemgetter
 
 class Song:
     
@@ -28,7 +30,7 @@ class Song:
         self.hmm.bake()
 
     def add_note_states(self):
-        for i, note in enumerate(notes):
+        for i, note in enumerate(self.notes):
             self.add_note(note, i)
 
     def add_note(self, note, i):
@@ -45,8 +47,12 @@ class Song:
         self.hmm.add_state(self.mistake_state)
 
     def add_mistake_transitions(self):
-        p_mistake_to_note = 1. / (len(self.note_states) + 1)
+        p_mistake_to_mistake = 1. - 1. / np.mean(self.durations)
+        p_mistake_to_note = (1. - p_mistake_to_mistake) / (len(self.note_states) + 1)
         p_note_to_mistake = 1. - self.p_correct
+        # mistake
+        self.hmm.add_transition(self.mistake_state, self.mistake_state, p_mistake_to_mistake)
+        # note states
         for note_state in self.note_states:
             self.hmm.add_transition(note_state, self.mistake_state, p_note_to_mistake)
             self.hmm.add_transition(self.mistake_state, note_state, p_mistake_to_note)
@@ -62,7 +68,7 @@ class Song:
             self.add_note_note_transition(state, next_state, duration)
         # start & end note states
         self.hmm.add_transition(self.hmm.start, self.note_states[0], self.p_correct)
-        self.hmm.add_transition(self.note_states[-1], self.hmm.end, 1. / durations[-1])
+        self.hmm.add_transition(self.note_states[-1], self.hmm.end, 1. / self.durations[-1])
 
     def add_note_note_transition(self, state, next_state, duration):
         # compute our transition probabilities
@@ -77,37 +83,42 @@ class Song:
         prob_path = self.hmm.predict_proba(pred_indices)
         self.state_index_path = np.argmax(prob_path, axis=1)
         self.state_path = [self.hmm.states[i] for i in self.state_index_path]
-        print "HMM prediction (state index: note index):"
-        print [state.name for state in self.state_path]
-        print "for RNN prediction:"
-        print pred_indices
-        
-def load_model(f_base):
-    model = model_from_json(open(f_base + '.json').read())
-    model.load_weights(f_base + '.h5')
-    return model
+        print "HMM prediction ('state index: note index', count):"
+        state_list = [state.name for state in self.state_path]
+        state_sum = [(key, len(list(group))) for key, group in groupby(state_list)]
+        print "\t{}".format(state_sum)
+        print "for RNN prediction (note, count):"
+        pred_sum = [(key, len(list(group))) for key, group in groupby(pred_indices)]
+        print "\t{}".format(pred_sum)
+
+if __name__ == '__main__':
+
+    def load_model(f_base):
+        model = model_from_json(open(f_base + '.json').read())
+        model.load_weights(f_base + '.h5')
+        return model
     
-s = Stairway(False)\
-    .step('load_audio', ['audio_file'], scipy.io.wavfile.read)\
-    .step('stft', ['load_audio'], stft, 0.1, 0.025)
+    s = Stairway(False)\
+        .step('load_audio', ['audio_file'], scipy.io.wavfile.read)\
+        .step('stft', ['load_audio'], stft, 0.1, 0.025)
 
-print "Loading audio file..."
-file_name = 'data/basic.wav'
-d = s.process(audio_file=file_name)
-d_train = np.zeros((1,)+d.shape)
-d_train[0] = d
+    print "Loading audio file..."
+    file_name = 'data/basic.wav'
+    d = s.process(audio_file=file_name)
+    d_train = np.zeros((1,)+d.shape)
+    d_train[0] = d
 
-print "Loading model..."
-model = load_model('models/model_1')
+    print "Loading model..."
+    model = load_model('models/model_1')
 
-print "Predicting with model..."
-pred = model.predict(d_train, batch_size=1)
-pred = np.squeeze(pred)
+    print "Predicting with model..."
+    pred = model.predict(d_train, batch_size=1)
+    pred = np.squeeze(pred)
 
-print "Predicting with HMM..."
-note_distribution_file = 'note_distribution.h5'
-notes = [88, 39, 41, 43, 44, 46, 88] # DOES THE AUDIO INCLUDE SILENCES
-bad_notes = [88, 39, 70, 43, 44, 46, 88]
-durations = [15., 10., 10., 10., 10., 10., 15.] 
-s = Song(bad_notes, durations, note_distribution_file)
-s.play(pred)
+    print "Predicting with HMM..."
+    note_distribution_file = 'note_distribution.h5'
+    notes = [88, 39, 41, 43, 44, 46, 88] # DOES THE AUDIO INCLUDE SILENCES
+    bad_notes = [88, 39, 70, 43, 44, 46, 88]
+    durations = [15., 10., 10., 10., 10., 10., 15.]
+    song = Song(bad_notes, durations, note_distribution_file)
+    song.play(pred)
